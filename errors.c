08605ecf7940c313beb7502c3c5ba77d32d4b554
HotelSierraWhiskey <winstontothemoon@gmail.com>
2021-10-01 01:34:16 -0400

    small cleanup

--- errors.c
+++ errors.c
@@ -5,3 +5,8 @@
 void error_file_not_found(void) {
     printf("Error: file not found\n");
 }
+
+
+void note_debug_output_file(const char *subsystem_name, const char *filename) {
+    printf("Note: %s in debug mode. Generating %s.\n", subsystem_name, filename);
+}
--- errors.h
+++ errors.h
@@ -4,5 +4,6 @@
 
 void error_file_not_found(void);
 
+void note_debug_output_file(const char *subsystem_name, const char *filename);
 
 #endif
