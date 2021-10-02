f3dcea446360aaafbd8a6154014d50f16dbb35e2
HotelSierraWhiskey <winstontothemoon@gmail.com>
2021-10-01 23:23:06 -0400

    added error_parser_token_mismatch()

--- errors.c
+++ errors.c
@@ -1,5 +1,6 @@
 #include "includes.h"
 #include "errors.h"
+#include "parser/parser.h"
 
 
 void error_file_not_found(void) {
@@ -7,6 +8,17 @@ void error_file_not_found(void) {
 }
 
 
+void error_parser_token_mismatch(token_type_t expected) {
+    printf("Error: got %s. expected %s \n", hash_token_type(parser.current_token->type), \
+    hash_token_type(expected));
+}
+
+
 void note_debug_output_file(const char *subsystem_name, const char *filename) {
     printf("Note: %s in debug mode. Generating %s.\n", subsystem_name, filename);
+}
+
+
+void note_trace_flag_set(void) {
+    printf("Note: TRACE flag is set\n");
 }
--- errors.h
+++ errors.h
@@ -2,8 +2,14 @@
 #define ERRORS_H
 
 
+#include "lexer/lexer.h"
+
 void error_file_not_found(void);
 
+void error_parser_token_mismatch(token_type_t expected);
+
 void note_debug_output_file(const char *subsystem_name, const char *filename);
+
+void note_trace_flag_set(void);
 
 #endif
