cb47b12527f51409dbb7baa3694adca40eb25709
HotelSierraWhiskey <winstontothemoon@gmail.com>
2021-10-07 01:04:55 -0400

    small cleanup

--- debug.c
+++ debug.c
@@ -17,7 +17,7 @@ debug_t debug;
 
 void parser_trace_callback(const char *caller_name) {
     #ifdef TRACE
-    printf("%-25s[%-20s\t%s]\n", caller_name, hash_token_type(parser.current_token->type), parser.current_token->contents);
+    printf("%-25s[token_type: %-18s\tcontents: %s]\n", caller_name, hash_token_type(parser.current_token->type), parser.current_token->contents);
     #endif
 }
 
@@ -29,7 +29,7 @@ extern void set_debug_flags(char **argv) {
     printf("\nTarget file: %s\n", system_info.filename);
     
     #ifdef TRACE
-    printf("Note: TRACE flag is set\n");
+    note_trace_flag_set();
     #endif
 
     for (char **c = argv; *c != NULL; c++) {
@@ -42,5 +42,4 @@ extern void set_debug_flags(char **argv) {
             note_debug_output_file("parser", pdebug_filename);
         }
     }
-    printf("\n");
 }
