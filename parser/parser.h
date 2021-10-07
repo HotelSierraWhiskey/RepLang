513e10a1cee60b5ebe69f6f7b7f3280c7f206bce
HotelSierraWhiskey <winstontothemoon@gmail.com>
2021-10-07 01:07:59 -0400

    small cleanup

--- parser/parser.h
+++ parser/parser.h
@@ -13,16 +13,6 @@ typedef struct {
 parser_t parser;
 
 
-// typedef struct _node_t {
-//     token_t *token;
-//     struct _node_t *left;
-//     struct _node_t *right;
-// } node_t;
-
-// extern node_t *root;
-
-
-
 extern void run_parser(void);
 
 extern void next_token(const char *caller_name);
@@ -33,7 +23,7 @@ extern bool check_tokens(token_type_t *types);
 
 extern bool check_peek(token_type_t type);
 
-extern bool match(token_type_t type);
+extern void match(token_type_t type);
 
 
 
@@ -51,20 +41,6 @@ void rule_term(void);
 void rule_unary(void);
 
 void rule_primary(void);
-
-
-
-
-
-// extern void tree_insert(token_t *token);
-
-// extern void pre_order_traversal(node_t *root);
-
-// extern void in_order_traversal(node_t *root);
-
-// extern void post_order_traversal(node_t *root);
-
-// void print_t(node_t *tree);
 
 
 #endif
